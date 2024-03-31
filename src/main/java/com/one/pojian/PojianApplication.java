f9d6a5478453b5c3443dcda76db764fdc6208731
Dreamice <dreamice13@foxmail.com>
2024-04-01 00:35:38 +0800

    init project

---
+++ src/main/java/com/one/pojian/PojianApplication.java
@@ -0,0 +1,13 @@
+package com.one.pojian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojianApplication.class, args);
+    }
+
+}
